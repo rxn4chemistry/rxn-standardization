cb03b4107a5a880ae7791524e60a8f027cc6a88c
Miruna Cretu <miruna.t.cretu@gmail.com>
2023-02-17 15:02:13 +0100

    restoring to initial commit

--- src/rxn-standardization/__init__.py
+++
@@ -1,6 +0,0 @@
-# LICENSED INTERNAL CODE. PROPERTY OF IBM.
-# IBM Research Zurich Licensed Internal Code
-# (C) Copyright IBM Corp. 2022
-# ALL RIGHTS RESERVED
-
-__version__ = "0.0.1"  # managed by bump2version
